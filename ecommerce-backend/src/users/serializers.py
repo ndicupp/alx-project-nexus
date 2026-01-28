9dc14906b4fe0b2ccefc8694eb2442beb930e2cf
Patrick NDICUNGUYE <ndipazzo43@gmail.com>
2026-01-28 19:49:33 +0200

    Update serializers.py

--- ecommerce-backend/src/users/serializers.py
+++ ecommerce-backend/src/users/serializers.py
@@ -1,18 +1,21 @@
 from rest_framework import serializers
-from .models import User
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
-class RegisterSerializer(serializers.ModelSerializer):
+class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ("email", "password")
+        fields = ['id', 'email', 'first_name', 'last_name', 'password']
 
     def create(self, validated_data):
+        # Using the manager's create_user ensures password hashing
         user = User.objects.create_user(
-            email=validated_data["email"],
-            password=validated_data["password"],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', '')
         )
         return user
-
