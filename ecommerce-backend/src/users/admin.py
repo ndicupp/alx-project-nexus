41e2ba7e909a825070467f1b806d0cbf88ecadda
Patrick NDICUNGUYE <ndipazzo43@gmail.com>
2026-01-28 10:00:43 +0200

    Update admin.py

--- ecommerce-backend/src/users/admin.py
+++ ecommerce-backend/src/users/admin.py
@@ -1,1 +1,34 @@
+#ecommerce-backend
+
+ecommerce-backend/
+├── docker/
+│   └── entrypoint.sh
+├── src/
+│   ├── core/
+│   │   ├── __init__.py
+│   │   ├── settings.py
+│   │   ├── urls.py
+│   │   ├── asgi.py
+│   │   └── wsgi.py
+│   ├── users/
+│   │   ├── migrations/
+│   │   ├── models.py
+│   │   ├── admin.py
+│   │   └── apps.py
+│   └── manage.py
+├── .env
+├── Dockerfile
+├── docker-compose.yml
+├── requirements.txt
+└── README.md
+
+docker-compose run backend django-admin startproject core src
+
+docker-compose run backend python src/manage.py startapp users
+
+#users/admin.py
+from django.contrib import admin
+from .models import User
+
+admin.site.register(User)
 
