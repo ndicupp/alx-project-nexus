f2e129870490fc2f26a7a04b8dcee7b3c0fbc754
Patrick NDICUNGUYE <ndipazzo43@gmail.com>
2026-01-28 19:55:33 +0200

    Update urls.py

--- ecommerce-backend/src/users/urls.py
+++ ecommerce-backend/src/users/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-from .views import RegisterView
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import RegisterView, LoginView
 
 urlpatterns = [
-    path("register/", RegisterView.as_view(), name="register"),
-    path("login/", TokenObtainPairView.as_view(), name="login"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
-
