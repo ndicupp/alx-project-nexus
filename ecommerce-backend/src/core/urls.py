bf6feaa3765127cf816666906d8ee7634fa3403a
Patrick NDICUNGUYE <ndipazzo43@gmail.com>
2026-01-28 19:57:51 +0200

    Update urls.py

--- ecommerce-backend/src/core/urls.py
+++ ecommerce-backend/src/core/urls.py
@@ -39,4 +39,15 @@ docker-compose up --build
 docker-compose exec web python manage.py makemigrations users
 docker-compose exec web python manage.py migrate
 
+from django.contrib import admin
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/auth/", include("users.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema")),
+]
+
 
