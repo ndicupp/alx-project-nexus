03ef10475f1c8f7ba8bb3f3f9dec3c8c648a99e6
Patrick NDICUNGUYE <ndipazzo43@gmail.com>
2026-01-28 09:58:32 +0200

    Update models.py

--- ecommerce-backend/src/users/models.py
+++ ecommerce-backend/src/users/models.py
@@ -1,1 +1,72 @@
+#ecommerce-backend
+
+ecommerce-backend/
+├── docker/
+│   └── entrypoint.sh
+├── src/
+│   ├── core/
+│   │   ├── __init__.py
+│   │   ├── settings.py
+│   │   ├── urls.py
+│   │   ├── asgi.py
+│   │   └── wsgi.py
+│   ├── users/
+│   │   ├── migrations/
+│   │   ├── models.py
+│   │   ├── admin.py
+│   │   └── apps.py
+│   └── manage.py
+├── .env
+├── Dockerfile
+├── docker-compose.yml
+├── requirements.txt
+└── README.md
+
+docker-compose run backend django-admin startproject core src
+
+docker-compose run backend python src/manage.py startapp users
+
+
+#users/models.py
+
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
 
