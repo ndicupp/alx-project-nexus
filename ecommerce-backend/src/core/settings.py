eba4473e4ee663dd35d08b04a40f8768cca7dc10
Patrick NDICUNGUYE <ndipazzo43@gmail.com>
2026-01-28 19:44:55 +0200

    Update settings.py

--- ecommerce-backend/src/core/settings.py
+++ ecommerce-backend/src/core/settings.py
@@ -67,3 +67,18 @@ SPECTACULAR_SETTINGS = {
 
 AUTH_USER_MODEL = "users.User"
 
+from datetime import timedelta
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "AUTH_HEADER_TYPES": ("Bearer",),
+}
+
+
