789512f0252bd5754baefa0b40f1dd4f79f50aa9
Patrick NDICUNGUYE <ndipazzo43@gmail.com>
2026-01-28 19:33:39 +0200

    Update models.py

--- ecommerce-backend/src/users/models.py
+++ ecommerce-backend/src/users/models.py
@@ -70,3 +70,24 @@ class User(AbstractBaseUser, PermissionsMixin):
         return self.email
 
 
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    username = None  # remove username field
+    email = models.EmailField(unique=True)
+
+    is_customer = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+
+
