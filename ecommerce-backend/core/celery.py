4cef66dde6f3ef2789e43ba3b88056042214d55d
Patrick NDICUNGUYE <ndipazzo43@gmail.com>
2026-01-28 19:21:23 +0200

    Update celery.py

--- ecommerce-backend/core/celery.py
+++ ecommerce-backend/core/celery.py
@@ -1,1 +1,7 @@
+import os
+from celery import Celery
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+app = Celery('nexus_backend')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
