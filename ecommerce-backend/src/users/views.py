fd460889efd3f4c91da0daa6200f034de263ffcd
Patrick NDICUNGUYE <ndipazzo43@gmail.com>
2026-01-28 19:51:19 +0200

    Update views.py

--- ecommerce-backend/src/users/views.py
+++ ecommerce-backend/src/users/views.py
@@ -1,1 +1,26 @@
+from rest_framework import status, generics
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import UserRegistrationSerializer
 
+class RegisterView(generics.CreateAPIView):
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [AllowAny] # Open to everyone
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({
+                "message": "User registered successfully",
+                "user": {
+                    "email": user.email,
+                    "id": user.id
+                }
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# We use the built-in SimpleJWT Login View
+class LoginView(TokenObtainPairView):
+    permission_classes = [AllowAny]
