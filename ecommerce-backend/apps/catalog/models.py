f72926c6d8f4220b1c50152e7152c76637d63127
Patrick NDICUNGUYE <ndipazzo43@gmail.com>
2026-01-29 18:01:25 +0200

    Update models.py

--- ecommerce-backend/apps/catalog/models.py
+++ ecommerce-backend/apps/catalog/models.py
@@ -1,1 +1,9 @@
+from django.db import models
+from django.utils.text import slugify
 
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True # This tells Django not to create a table for this model
