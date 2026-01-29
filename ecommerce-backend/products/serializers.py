e6008d2f07f5c67e1196c9db3894850268f770ea
Patrick NDICUNGUYE <ndipazzo43@gmail.com>
2026-01-29 17:34:00 +0200

    Update serializers.py

--- ecommerce-backend/products/serializers.py
+++ ecommerce-backend/products/serializers.py
@@ -1,1 +1,20 @@
+from rest_framework import serializers
+from .models import Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source="category.name", read_only=True)
+
+    class Meta:
+        model = Product
+        fields = [
+            "id",
+            "name",
+            "description",
+            "price",
+            "stock",
+            "is_active",
+            "category",
+            "category_name",
+        ]
 
