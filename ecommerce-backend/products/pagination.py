1bb63b181ec82df558d97077f9c59639434d8d67
Patrick NDICUNGUYE <ndipazzo43@gmail.com>
2026-01-29 21:31:20 +0200

    Update pagination.py

--- ecommerce-backend/products/pagination.py
+++ ecommerce-backend/products/pagination.py
@@ -1,1 +1,10 @@
+from rest_framework.pagination import PageNumberPagination
+
+
+class ProductPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+    max_page_size = 50
+
+pagination_class = ProductPagination
 
