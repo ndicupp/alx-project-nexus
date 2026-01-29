fcdbd91c49a948f423457d20cb546001f969e230
Patrick NDICUNGUYE <ndipazzo43@gmail.com>
2026-01-29 17:26:44 +0200

    Update admin.py

--- ecommerce-backend/products/admin.py
+++ ecommerce-backend/products/admin.py
@@ -3,3 +3,6 @@ from .models import Product
 
 admin.site.register(Product)
 
+
+docker-compose run backend python src/manage.py makemigrations
+docker-compose run backend python src/manage.py migrate
