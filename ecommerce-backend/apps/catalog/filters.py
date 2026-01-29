6766cde955d382ad9fe78499233e26cc9912087c
Patrick NDICUNGUYE <ndipazzo43@gmail.com>
2026-01-29 21:08:54 +0200

    Update filters.py

--- ecommerce-backend/apps/catalog/filters.py
+++ ecommerce-backend/apps/catalog/filters.py
@@ -1,1 +1,14 @@
+from django_filters import rest_framework as filters
+from .models import Product
 
+class ProductFilter(filters.FilterSet):
+    # Allow users to find products between two prices
+    min_price = filters.NumberFilter(field_name="price", lookup_expr='gte')
+    max_price = filters.NumberFilter(field_name="price", lookup_expr='lte')
+    
+    # Filter by category name instead of just ID
+    category_name = filters.CharFilter(field_name="category__name", lookup_expr='icontains')
+
+    class Meta:
+        model = Product
+        fields = ['category', 'min_price', 'max_price', 'category_name']
