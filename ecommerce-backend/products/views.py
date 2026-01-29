0f8834a4e4d8425a86663274ecc4aff773441254
Patrick NDICUNGUYE <ndipazzo43@gmail.com>
2026-01-29 17:46:20 +0200

    Update views.py

--- ecommerce-backend/products/views.py
+++ ecommerce-backend/products/views.py
@@ -1,1 +1,13 @@
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.filter(is_active=True)
+    serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ["category"]
+    ordering_fields = ["price"]
 
