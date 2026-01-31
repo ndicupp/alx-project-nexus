c02f6a97fcb39bd94db305428c302cfe258bdba0
Patrick NDICUNGUYE <ndipazzo43@gmail.com>
2026-01-31 10:35:52 +0200

    Update test_models.py

--- ecommerce-backend/products/tests/test_models.py
+++ ecommerce-backend/products/tests/test_models.py
@@ -1,1 +1,15 @@
+import pytest
+from products.models import Product, Category
+
+@pytest.mark.django_db
+def test_product_creation():
+    category = Category.objects.create(name="Electronics", slug="electronics")
+    product = Product.objects.create(
+        category=category,
+        name="Laptop",
+        price=1200,
+        stock=10
+    )
+    assert product.name == "Laptop"
+    assert product.category.slug == "electronics"
 
